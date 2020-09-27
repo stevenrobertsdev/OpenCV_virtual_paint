23ac38474e82440ce439217662f6212fff34fede
Steven <contact@stevenroberts.info>
2020-09-27 16:10:26 +0100

    feat: add text change on color change

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-frame_width = 840
+frame_width = 640
 frame_height = 480
 
 capture = cv2.VideoCapture(0)
@@ -12,37 +12,45 @@ capture.set(10, 150)
 
 # sets of the colors to detect - hMin, sMin, vMin, hMax, sMax, vMax
 color_sets = [[165, 151, 0, 179, 255, 255],
-              [97, 192, 51, 119, 255, 255]]
+              [97, 192, 51, 119, 255, 255],
+              [82, 125, 18, 112, 255, 97]]
 
 # color values written in BGR not RGB
 color_values = [[0, 0, 255],
-                [255, 0, 0]]
+                [255, 0, 0],
+                [0, 255, 0]]
+
+color_names = ['RED', 'BLUE', 'GREEN']
 
 
-def find_color(cap, colors, values):
+def find_color(cap, colors, values, names):
     capture_hsv = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV)
     count = 0
     for color in colors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(capture_hsv, lower, upper)
-        x, y = get_contours(mask)
+        x, y, show_text = get_contours(mask)
         cv2.circle(capture_result, (x, y), 10, values[count], cv2.FILLED)
+
+        if show_text:
+            cv2.putText(capture_result, names[count], (320, 400), cv2.FONT_HERSHEY_COMPLEX, 2, values[count], 2)
+
         count += 1
         # cv2.imshow(str(color[0]), mask)
 
 
 def get_contours(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    x, y, w, h = 0, 0, 0, 0
+    x, y, w, h, show_text = 0, 0, 0, 0, False
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > 500:
-            # cv2.drawContours(capture_result, contour, -1, (255, 0, 179), 5)
             peri = cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
             x, y, w, h = cv2.boundingRect(approx)
-    return x + w // 2, y
+            show_text = True
+    return x + w // 2, y, show_text
 
 
 while True:
@@ -51,7 +59,7 @@ while True:
     # Create copy of frames
     capture_result = frame.copy()
     # apply find color and contours
-    find_color(frame, color_sets, color_values)
+    find_color(frame, color_sets, color_values, color_names)
     # display frames
     cv2.imshow('Result', capture_result)
 
